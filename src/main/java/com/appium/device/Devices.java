fe4e98856ca97ade91377d587510bc761bca2c47
jpatel-oss <jpatel@suki.ai>
2026-02-06 16:24:04 +0530

    Added Local and Devicelab

--- src/main/java/com/appium/device/Devices.java
+++ src/main/java/com/appium/device/Devices.java
@@ -1,13 +1,17 @@
 package com.appium.device;
 
+import com.appium.capabilities.Capabilities;
 import com.appium.manager.AppiumServerManager;
 import com.appium.utils.Api;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
+import org.json.JSONObject;
 
 import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
+
+import static com.appium.utils.OverriddenVariable.getOverriddenStringValue;
 
 public class Devices {
     private static List<Device> instance;
@@ -19,13 +23,30 @@ public class Devices {
     @SneakyThrows
     public static List<Device> getConnectedDevices() {
         if (instance == null) {
-            System.out.println(Thread.currentThread().getId());
-            AppiumServerManager appiumServerManager = new AppiumServerManager();
-            String remoteWDHubIP = appiumServerManager.getRemoteWDHubIP();
-            URL url = new URL(remoteWDHubIP);
-            String response = new Api().getResponse(url.getProtocol()
-                    + "://" + url.getHost() + ":" + url.getPort() + "/device-farm/api/device");
-            instance =  Arrays.asList(new ObjectMapper().readValue(response, Device[].class));
+            String deviceToExecute = getOverriddenStringValue("DEVICE_TO_EXECUTE", "local");
+
+            if ("devicefarm".equalsIgnoreCase(deviceToExecute)) {
+                // Get hub URL from capabilities
+                JSONObject serverConfig = Capabilities.getInstance()
+                        .getCapabilityObjectFromKey("serverConfig");
+                String hubUrl = serverConfig
+                        .getJSONObject("server")
+                        .getJSONObject("plugin")
+                        .getJSONObject("device-farm")
+                        .getString("hub");
+
+                // Make API call to hub
+                String response = new Api().getResponse(hubUrl + "/device-farm/api/device");
+                instance = Arrays.asList(new ObjectMapper().readValue(response, Device[].class));
+            } else {
+                // Existing local logic
+                AppiumServerManager appiumServerManager = new AppiumServerManager();
+                String remoteWDHubIP = appiumServerManager.getRemoteWDHubIP();
+                URL url = new URL(remoteWDHubIP);
+                String response = new Api().getResponse(url.getProtocol()
+                        + "://" + url.getHost() + ":" + url.getPort() + "/device-farm/api/device");
+                instance = Arrays.asList(new ObjectMapper().readValue(response, Device[].class));
+            }
         }
         return instance;
     }
